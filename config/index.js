cb10eec2fdfd133888de25d4915934419ae5626f
jgwangboshu <boshu.wang@jgdt.com>
2022-03-20 01:27:01 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: './',
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
