21b565d69e2ed1f7d41f0325d5e1e36f6dc95e2a
jgwangboshu <boshu.wang@jgdt.com>
2022-03-20 00:26:07 +0800

    修改路径

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: './',
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
